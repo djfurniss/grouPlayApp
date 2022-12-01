8937abe4588d65be070dfc322a1b3409005b3ea2
Denasia <dfurniss@ymail.com>
2022-12-01 18:52:45 -0500

    welcome replaced with grouPlay at top of dashboard

--- app/screens/Dashboard.js
+++ app/screens/Dashboard.js
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { SafeAreaView, Text, StyleSheet } from "react-native";
+import { SafeAreaView, View, Text, StyleSheet } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { getPlaylists } from "../utils/api";
 
 export default function Dashboard({ route }){
-    const { user : {user_name} } = route.params
-    const [isLoading, setIsLoading] = useState(false)
+    const { user } = route.params
+    const [isLoading, setIsLoading] = useState(false);
+    const [playlists, setPlaylists] = useState([]);
 
     useEffect(()=>{
         setIsLoading(true)
-        const loadDashboard = () => {
-
+        const loadDashboard = async() => {
+            getPlaylists(user.user_id)
+                .then(setPlaylists)
         };
 
         loadDashboard();
@@ -19,10 +21,22 @@ export default function Dashboard({ route }){
     return( 
         <SafeAreaView style={styles.container}>
             <LinearGradient
-                    // Background Linear Gradient
-                    colors={['#03045E', '#0077B6']}
-                    style={styles.background}/>
-            <Text>Welcome, {user_name}</Text>
+                // Background Linear Gradient
+                colors={['#03045E', '#0077B6']}
+                style={styles.background}/>
+
+            <Text style={styles.name}>grouPlay</Text>
+
+            <View style={styles.playlistContainer}>
+                {playlists.length ? playlists.map(playlist => {
+                    return (
+                        <View key={playlist.playlist_id} style={styles.playlist}>
+                            <Text>{playlist.playlist_name}</Text>
+                        </View>
+                    )
+                }): <Text>Add your first playlist!</Text>}
+            </View>
+                
         </SafeAreaView>
     )
 };
@@ -31,7 +45,7 @@ const styles = StyleSheet.create({
     container:{
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center',
+        // justifyContent: 'center',
     },
     background: {
         position: 'absolute',
@@ -39,5 +53,25 @@ const styles = StyleSheet.create({
         right: 0,
         top: 0,
         bottom: 0,
-    }
+    },
+    name:{
+        color: '#fff',
+        fontSize: 40,
+        marginTop: 30,
+    },
+    playlistContainer: {
+        alignSelf: 'stretch',
+        display: 'flex',
+        flexDirection: 'column',
+        marginLeft: 30,
+        marginRight: 30,
+        marginTop: 20,
+    },
+    playlist: {
+        backgroundColor: 'white',
+        borderRadius: '10%',
+        margin: 5,
+        padding: 10,
+        height: 100,
+    },
 })
