26eb049fed8b897003e3a97456167e2be77f331a
Denasia <dfurniss@ymail.com>
2022-12-01 18:57:32 -0500

    Home checks for users credentials in storage, renders Dashboard if so

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text, Button, StyleSheet, StatusBar } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { getStoredUser } from "../utils/storedUser";
+import { logIn } from "../utils/api";
 
 export default function Home({ navigation }){
+    useEffect(()=>{
+       getStoredUser()
+        .then(({username, password})=>{
+            if(username && password){
+                console.log("your stored information is being loaded")
+                logIn(username, password)
+                .then(({data})=> navigation.navigate("Dashboard", {user: data}))
+            }else{
+                console.log("your log in info isn't stored")
+            }
+        })
+    },[])
+
     return (
         <View style={styles.container}>
             <StatusBar barStyle={'light-content'}/>
@@ -10,9 +25,7 @@ export default function Home({ navigation }){
                 // Background Linear Gradient
                 colors={['#03045E', '#0077B6']}
                 style={styles.background}
-            >
-
-            </LinearGradient>
+            />
             <Text style={styles.name}>grouPlay</Text>
 
              <View style={styles.login}>
@@ -23,7 +36,8 @@ export default function Home({ navigation }){
             </View>
             <View style={styles.register}>
                 <Button
-                    title='sign up'
+                    title='register'
+                    onPress={() => navigation.navigate('Register')}
                     color='#0077B6'/>
             </View>
         </View>
