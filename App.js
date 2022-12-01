43e23759db4646a0d6509131859666053f8445d1
Denasia <dfurniss@ymail.com>
2022-12-01 18:59:14 -0500

    switched from Stack Native to Bottom Tabs nav at the botton, rendered icons using rneui

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer} from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "./app/screens/Home.js";
 import Login from "./app/screens/Login";
+import Register from "./app/screens/Register.js";
 import Dashboard from "./app/screens/Dashboard.js";
+import Settings from "./app/screens/Settings.js";
+import { Icon } from '@rneui/themed';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
   
 export default function App() {
+    // ? is there a way to get the linear gradient higher up so I don't have to declare it on everypage's styling?? 
     return(
         <NavigationContainer>
-            <Stack.Navigator screenOptions={{headerShown: false}}>
-                <Stack.Screen name="Home" component={Home}/>
-                <Stack.Screen name="Login" component={Login}/>
-                <Stack.Screen name="Dashboard" component={Dashboard}/>
-            </Stack.Navigator>
+        <Tab.Navigator 
+            screenOptions={{headerShown: false, tabBarStyle: 
+                {display: 'flex', 
+                position: 'absolute', 
+                backgroundColor: '#00000050',
+                borderTopWidth: 0,
+            }, tabBarShowLabel: false}} 
+            backBehavior={'history'}>
+            <Tab.Screen name="Home" component={Home} options={{tabBarStyle: {display:'none'}, tabBarItemStyle: {display: 'none'}}}/>
+            <Tab.Screen name="Login" component={Login} options={{tabBarStyle: {display:'none'}, tabBarItemStyle: {display: 'none'}}}/>
+            <Tab.Screen name="Register" component={Register} options={{tabBarStyle: {display:'none'}, tabBarItemStyle: {display: 'none'}}}/>
+            <Tab.Screen 
+                name="Dashboard" 
+                component={Dashboard} 
+                options={
+                    {tabBarStyle: 
+                        {display: 'flex', 
+                        position: 'absolute', 
+                        backgroundColor: '#00000050',
+                        borderTopWidth: 0,
+                    },
+            
+                tabBarIcon: () => <Icon name="apps-outline" type="ionicon" color={'#fff'}/>
+                }}/>
+            <Tab.Screen 
+                name="Settings" 
+                component={Settings} 
+                options={
+                    {tabBarStyle: 
+                        {display: 'flex', 
+                        position: 'absolute', 
+                        backgroundColor: '#00000050',
+                        borderTopWidth: 0,
+                    },
+                tabBarIcon: () => <Icon name="settings-outline" type="ionicon" color={'#fff'}/>
+                    }}/>
+        </Tab.Navigator>
         </NavigationContainer>
     )
 };
