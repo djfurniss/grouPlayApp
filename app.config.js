d4604ae5c2f0ae8ba0fddbc57fc9a2b770e323ec
Denasia <dfurniss@ymail.com>
2022-12-01 22:56:32 -0500

    had to set extra.eas.projectId

--- app.config.js
+++ app.config.js
@@ -3,6 +3,9 @@ module.exports = {
     version: '1.0.0',
     extra: {
       BASE_API_URL: process.env.BASE_API_URL,
+      "eas": {
+        "projectId": "d9a5a875-2850-4346-8239-52d81044106c"
+      },
     },
   };
   
