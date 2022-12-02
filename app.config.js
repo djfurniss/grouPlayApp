1a14b122396b91ffc4e5d880fc05df9584c69077
Denasia <dfurniss@ymail.com>
2022-12-01 22:57:46 -0500

    updates and runtimeversion added to app.config.js

--- app.config.js
+++ app.config.js
@@ -7,5 +7,11 @@ module.exports = {
         "projectId": "d9a5a875-2850-4346-8239-52d81044106c"
       },
     },
+    "updates": {
+        "url": "https://u.expo.dev/d9a5a875-2850-4346-8239-52d81044106c"
+      },
+    "runtimeVersion": {
+        "policy": "sdkVersion"
+    }
   };
   
