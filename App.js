e976472414a96c6665e45f5de92906103e0344f2
Denasia <dfurniss@ymail.com>
2022-12-10 22:22:46 -0600

    using Tab.Navigator instead of screen for bottom bar nav

--- App.js
+++ App.js
@@ -1,56 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavigationContainer} from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "./app/screens/Home.js";
 import Login from "./app/screens/Login";
 import Register from "./app/screens/Register.js";
 import Dashboard from "./app/screens/Dashboard.js";
+import New from "./app/screens/New.js";
+import Playlist from "./app/screens/Playlist.js";
+import Friends from "./app/screens/Friends.js";
 import Settings from "./app/screens/Settings.js";
+import { UserContext } from "./app/contexts/UserContext";
 import { Icon } from '@rneui/themed';
 
 const Tab = createBottomTabNavigator();
   
 export default function App() {
     // ? is there a way to get the linear gradient higher up so I don't have to declare it on everypage's styling?? 
+    const [user, setUser] = useState(null)
+    const [playlists, setPlaylists] = useState([])
+
     return(
         <NavigationContainer>
-        <Tab.Navigator 
-            screenOptions={{headerShown: false, tabBarStyle: 
-                {display: 'flex', 
-                position: 'absolute', 
-                backgroundColor: '#00000050',
-                borderTopWidth: 0,
-            }, tabBarShowLabel: false}} 
-            backBehavior={'history'}>
-            <Tab.Screen name="Home" component={Home} options={{tabBarStyle: {display:'none'}, tabBarItemStyle: {display: 'none'}}}/>
-            <Tab.Screen name="Login" component={Login} options={{tabBarStyle: {display:'none'}, tabBarItemStyle: {display: 'none'}}}/>
-            <Tab.Screen name="Register" component={Register} options={{tabBarStyle: {display:'none'}, tabBarItemStyle: {display: 'none'}}}/>
-            <Tab.Screen 
-                name="Dashboard" 
-                component={Dashboard} 
-                options={
-                    {tabBarStyle: 
+            <UserContext.Provider value={{user, setUser, playlists, setPlaylists}}>
+                <Tab.Navigator 
+                    screenOptions={{headerShown: false, tabBarStyle: 
                         {display: 'flex', 
                         position: 'absolute', 
                         backgroundColor: '#00000050',
                         borderTopWidth: 0,
-                    },
-            
-                tabBarIcon: () => <Icon name="apps-outline" type="ionicon" color={'#fff'}/>
-                }}/>
-            <Tab.Screen 
-                name="Settings" 
-                component={Settings} 
-                options={
-                    {tabBarStyle: 
-                        {display: 'flex', 
-                        position: 'absolute', 
-                        backgroundColor: '#00000050',
-                        borderTopWidth: 0,
-                    },
-                tabBarIcon: () => <Icon name="settings-outline" type="ionicon" color={'#fff'}/>
-                    }}/>
-        </Tab.Navigator>
+                    }, tabBarShowLabel: false}} 
+                    backBehavior={'history'}>
+                    <Tab.Screen name="Home" component={Home} options={{tabBarStyle: {display:'none'}, tabBarItemStyle: {display: 'none'}}}/>
+                    <Tab.Screen name="Login" component={Login} options={{tabBarStyle: {display:'none'}, tabBarItemStyle: {display: 'none'}}}/>
+                    <Tab.Screen name="Register" component={Register} options={{tabBarStyle: {display:'none'}, tabBarItemStyle: {display: 'none'}}}/>
+                    
+                    <Tab.Screen 
+                        name="Friends" 
+                        component={Friends} 
+                        options={
+                            {tabBarStyle: 
+                                {display: 'flex', 
+                                position: 'absolute', 
+                                backgroundColor: '#00000050',
+                                borderTopWidth: 0,
+                            },
+                        tabBarIcon: () => <Icon name="people-outline" type="ionicon" color={'#fff'}/>
+                            }}/>
+
+                    <Tab.Screen 
+                        name="Dashboard" 
+                        component={Dashboard} 
+                        options={
+                            {tabBarStyle: 
+                                {display: 'flex', 
+                                position: 'absolute', 
+                                backgroundColor: '#00000050',
+                                borderTopWidth: 0,
+                            },
+                    
+                        tabBarIcon: () => <Icon name="home-outline" type="ionicon" color={'#fff'}/>
+                        }}/>
+                    
+                    <Tab.Screen 
+                        name="Settings" 
+                        component={Settings} 
+                        options={
+                            {tabBarStyle: 
+                                {display: 'flex', 
+                                position: 'absolute', 
+                                backgroundColor: '#00000050',
+                                borderTopWidth: 0,
+                            },
+                        tabBarIcon: () => <Icon name="settings-outline" type="ionicon" color={'#fff'}/>
+                            }}/>
+                    <Tab.Screen 
+                        name="New"
+                        component={New}
+                        options={{tabBarItemStyle: {display: 'none'}}}/>
+
+                    <Tab.Screen 
+                        name="Playlist"
+                        component={Playlist}
+                        options={{tabBarItemStyle: {display: 'none'}}}/>
+
+                </Tab.Navigator>
+            </UserContext.Provider>
         </NavigationContainer>
     )
 };
